ecaec14f4c5e332e33dc9c70f1db61c791b34f77
Cavalex <mateusmpta@gmail.com>
2023-02-23 21:48:28 +0000

    ex 1,2,3 done

--- TPC1/main.py
+++ TPC1/main.py
@@ -32,6 +32,8 @@ def distGender(people):
     disFemale = 0
     totalMale = 0
     totalFemale = 0
+
+    # count everything
     for p in people:
         totalMale = totalMale+1 if p.gender == "M" else totalMale
         totalFemale = totalFemale+1 if p.gender == "F" else totalFemale
@@ -41,22 +43,64 @@ def distGender(people):
             else:
                 disFemale += 1
 
-    print("\nH com doença:", disMale, "de um total de", totalMale)
+    print("\nDistribuição por género:")
+    print("H com doença:", disMale, "de um total de", totalMale)
     print("com uma percentagem de", round(disMale/totalMale*100, 1), "% homens infetados")
     print("para um total de homens infetados sobre a a população total de", round(disMale/(totalMale+totalFemale)*100, 1), "%")
-    print("\nM com doença:", disFemale, "de um total de", totalFemale)
+    print("M com doença:", disFemale, "de um total de", totalFemale)
     print("com uma percentagem de", round(disFemale/totalFemale*100, 1), "% mulheres infetadas")
     print("para um total de mulheres infetadas sobre a a população total de", round(disFemale/(totalMale+totalFemale)*100, 1), "%")
 
     return (disMale, disFemale, totalMale, totalFemale)
 
 def distAge(people):
-    pass
+    ageSmallest = int(people[0].age)
+    ageBiggest = int(people[0].age)
+
+    # get the smallest and biggest ages
+    for p in people:
+        ageSmallest = int(p.age) if int(p.age) < ageSmallest else ageSmallest
+        ageBiggest = int(p.age) if int(p.age) > ageBiggest else ageBiggest
+    
+    print(type(ageSmallest))
+    print(type(ageBiggest))
+
+    ageSmallest = floor_to_multiple(ageSmallest, 5) # se for 26 p.ex, passa para 25
+    ageBiggest = ceil_to_multiple(ageBiggest, 5) # se for 76 p.ex, passa para 80
+
+    # create the age groups
+    ageGroups = {}
+    for i in range(len(people)):
+        age = int(people[i].age)
+        ageGroup = floor_to_multiple(age, 5) # get the age group of this person
+        ageGroup = str(ageGroup) + "-" + str(ageGroup+4)
+        if ageGroup in ageGroups:
+            ageGroups[ageGroup].append(people[i])
+        else:
+            ageGroups[ageGroup] = [people[i]]
+
+    # count the number of people in each age group with disease
+    for ageGroup in ageGroups:
+        count = 0
+        for p in ageGroups[ageGroup]:
+            if p.disease == "1":
+                count += 1
+        ageGroups[ageGroup] = count
+
+    # sort ageGroups by key
+    ageGroups = dict(sorted(ageGroups.items(), key=lambda item: item[0]))
+
+    print("\nDistribuição por faixa etária:")
+    print(ageGroups)
+
 
 def main():
-    data = loadData()
-    people = loadPeople(data)
-    disMale, disFemale, totalMale, totalFemale = distGender(people)
+    data = loadData() # 1
+    people = loadPeople(data) # 1
+    disMale, disFemale, totalMale, totalFemale = distGender(people) # 2
+    distAge(people) # 3
+
+
 
 if __name__ == "__main__":
     main()
