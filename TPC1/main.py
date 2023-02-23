7730c2908cfa917edcec34a524ec456463cf47bc
Cavalex <mateusmpta@gmail.com>
2023-02-23 23:15:00 +0000

    finished tpc1

--- TPC1/main.py
+++ TPC1/main.py
@@ -1,5 +1,7 @@
 from person import Person
 from math import floor, ceil
+import matplotlib.pyplot as plt
+import matplotlib
 
 def ceil_to_multiple(number, multiple):
     return multiple * ceil(number / multiple)
@@ -43,17 +45,19 @@ def distGender(people):
             else:
                 disFemale += 1
 
-    print("\nDistribuição por género:")
-    print("H com doença:", disMale, "de um total de", totalMale)
-    print("com uma percentagem de", round(disMale/totalMale*100, 1), "% homens infetados")
-    print("para um total de homens infetados sobre a a população total de", round(disMale/(totalMale+totalFemale)*100, 1), "%")
-    print("M com doença:", disFemale, "de um total de", totalFemale)
-    print("com uma percentagem de", round(disFemale/totalFemale*100, 1), "% mulheres infetadas")
-    print("para um total de mulheres infetadas sobre a a população total de", round(disFemale/(totalMale+totalFemale)*100, 1), "%")
-
-    return (disMale, disFemale, totalMale, totalFemale)
+    dist = {
+        "M":totalMale,
+        "F":totalFemale,
+        "Total":totalFemale+totalMale,
+        "M_infected":disMale,
+        "F_infected":disFemale,
+        "Total_infected":disMale+disFemale
+    }
+    print(dist)
+    return dist
 
 def distAge(people):
+    print("\nDistribuição por faixa etária:")
     ageSmallest = int(people[0].age)
     ageBiggest = int(people[0].age)
 
@@ -62,11 +66,11 @@ def distAge(people):
         ageSmallest = int(p.age) if int(p.age) < ageSmallest else ageSmallest
         ageBiggest = int(p.age) if int(p.age) > ageBiggest else ageBiggest
     
-    print(type(ageSmallest))
-    print(type(ageBiggest))
+    print("Youngest person is:", ageSmallest, "years old")
+    print("Oldest person is:", ageBiggest, "years old")
 
-    ageSmallest = floor_to_multiple(ageSmallest, 5) # se for 26 p.ex, passa para 25
-    ageBiggest = ceil_to_multiple(ageBiggest, 5) # se for 76 p.ex, passa para 80
+    ageSmallest = floor_to_multiple(ageSmallest, 5) # se for 28 p.ex, passa para 25
+    ageBiggest = ceil_to_multiple(ageBiggest, 5) # se for 77 p.ex, passa para 80
 
     # create the age groups
     ageGroups = {}
@@ -89,18 +93,95 @@ def distAge(people):
 
     # sort ageGroups by key
     ageGroups = dict(sorted(ageGroups.items(), key=lambda item: item[0]))
-
-    print("\nDistribuição por faixa etária:")
     print(ageGroups)
 
+    return ageGroups
+
+def distColesterol(people):
+    print("\nDistribuição por faixa etária:")
+
+    # remove people with 0 colesterol
+    people_colesterol = []
+    for p in people:
+        if int(p.colesterol) > 0: 
+            people_colesterol.append(p)
+    colSmallest = int(people[0].colesterol)
+    colBiggest = int(people[0].colesterol)
+
+    # get the smallest and biggest ages
+    for p in people_colesterol:
+        colSmallest = int(p.colesterol) if int(p.colesterol) < colSmallest else colSmallest
+        colBiggest = int(p.colesterol) if int(p.colesterol) > colBiggest else colBiggest
+    
+    print("Youngest person is:", colSmallest, "years old")
+    print("Oldest person is:", colBiggest, "years old")
+
+    colSmallest = floor_to_multiple(colSmallest, 10) # se for 28 p.ex, passa para 25
+    colBiggest = ceil_to_multiple(colBiggest, 10) # se for 77 p.ex, passa para 80
+
+    # create the age groups
+    colGroups = {}
+    for i in range(len(people_colesterol)):
+        col = int(people_colesterol[i].colesterol)
+        colGroup = floor_to_multiple(col, 10) # get the age group of this person
+        colGroup = str(colGroup) + "-" + str(colGroup+9)
+        if colGroup in colGroups:
+            colGroups[colGroup].append(people_colesterol[i])
+        else:
+            colGroups[colGroup] = [people_colesterol[i]]
+
+    # count the number of people in each age group with disease
+    for colGroup in colGroups:
+        count = 0
+        for p in colGroups[colGroup]:
+            if p.disease == "1":
+                count += 1
+        colGroups[colGroup] = count
+
+    # sort ageGroups by key
+    colGroups = dict(sorted(colGroups.items(), key=lambda item: item[0]))
+    print(colGroups)
+
+    return colGroups
+
+def dictToTable(d_list):
+    for d in d_list:
+        l1 = 0
+        l2 = 0
+        for k, v in d.items():
+            l1 = len(str(k)) if len(str(k)) > l1 else l1
+            l2 = len(str(v)) if len(str(v)) > l2 else l2
+        
+        print("_"*(l1+l2+9))
+        for p in d:
+            print("|", p, " "*(l1-len(str(p))), "|", d[p], " "*(l2-len(str(d[p]))), "|")
+        print("-"*(l1+l2+9))
+
+def plotDist(d_list):
+    for d, title in d_list:
+        plt.close(fig="all") # close all previous plots
+        matplotlib.rcParams.update({'font.size': 7})
+        if len(d) > 10:
+            plt.xticks(rotation=75) # rotate x axis labels
+        plt.bar(d.keys(), d.values())
+
+        plt.title(title)
+        fileToSave = f"{title}.png"
+        plt.savefig(fileToSave)
 
 def main():
     data = loadData() # 1
     people = loadPeople(data) # 1
-    disMale, disFemale, totalMale, totalFemale = distGender(people) # 2
-    distAge(people) # 3
-
-
+    #disMale, disFemale, totalMale, totalFemale = distGender(people) # 3
+    genderGroups = distGender(people) # 3
+    ageGroups = distAge(people) # 4
+    colGroups = distColesterol(people) # 5
+    dictToTable([genderGroups, ageGroups, colGroups]) # 6 e 7
+    plotDist([
+        (genderGroups, "gender_distribution"), 
+        (ageGroups, "age_distribution"), 
+        (colGroups, "colesterol_distribution")
+    ]) # 8, draw the plots
 
 if __name__ == "__main__":
     main()
